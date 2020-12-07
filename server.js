07a9f72c2aa9109e13a307c4012533b29c32bee4
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-12-07 13:22:39 +0100

    cors

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+const express = require("express");
+const app = express();
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
