e9c50cd6a7708077f427e3874b4e9565c2c42103
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-12-07 14:44:14 +0100

    fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://www.google.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
