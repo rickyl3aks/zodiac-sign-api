0af7d9067075e399650c2a78564645b2120e4f6f
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-12-07 16:12:45 +0100

    fixes

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin: http://www.google.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+
   next();
 });
 
