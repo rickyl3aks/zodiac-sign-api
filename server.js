4d8ee77d774abfa6c367b6f5c01d03157b544aef
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-12-08 18:00:23 +0100

    ok

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
-const express = require("express");
-const app = express();
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
-app.use(cors());
-
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
 
 server.use(middlewares);
 server.use(router);
